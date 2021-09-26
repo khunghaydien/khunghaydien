521e87fef9116039ca10cc9af90381fa42408b44
Phùng Hữu Đạt <79575713+khunghaydien@users.noreply.github.com>
2021-09-26 13:03:55 +0700

    Add files via upload

---
+++ hopdong (1).php
@@ -0,0 +1,337 @@
+<?php
+// $today = date("Y-m-d-His");
+// header("Content-type: application/vnd.ms-word");
+// header("Content-Disposition: attachment;Filename=Invoice-".$today.".doc");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Hợp đồng hợp tác bác sỹ</title>
+    <meta charset="UTF-8">
+    <style>
+    .center {
+        text-align: center;
+        font-weight: bolder;
+    }
+
+    .title,
+    .highlight {
+        font-weight: bolder;
+    }
+
+    .dieukhoan,
+    .ngay {
+        padding: 0 10%;
+    }
+
+    table {
+        margin-right: 100px;
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    .table-ky td {
+        text-align: center;
+    }
+
+    .table-ky td p {
+        margin-bottom: 100px;
+    }
+
+    .container {
+        padding: 0 12%;
+    }
+
+    .ten {
+        padding: 0px 25px;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="center">
+        CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM<br> Độc lập – Tự do – Hạnh phúc</br>
+        ----****----
+        <p class="hopdong">HỢP ĐỒNG HỢP TÁC </p>
+
+    </div>
+    <p class="ngay">Hôm nay, ngày ...... tháng …... năm 2021</p>
+    <div class="container">
+        <table border="1">
+            <tr>
+                <td>
+                    <p class="ten">
+                        BÊN A: </br> Tên Đơn vị</br>
+                        Địa chỉ </br>Do Ông/ Bà</br> Chức vụ
+                    </p>
+                </td>
+                <td>
+
+                    :
+                    ….............................................................................................................
+                    </br>
+                    :
+                    ….............................................................................................................
+                    </br>
+                    :
+                    ….............................................................................................................
+                    </br>
+                    :
+                    ….............................................................................................................
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p class="ten">
+                        BÊN B </br> Ông/ Bà </br> Ngày sinh </br> CMND số </br>Địa chỉ thường trú
+                    </p>
+
+                </td>
+                <td>
+
+                    :
+                    ….............................................................................................................
+                    </br>
+                    :
+                    ….............................................................................................................
+                    </br>
+                    :
+                    ….............................................................................................................
+                    </br>
+                    :
+                    ….............................................................................................................
+
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="dieukhoan">
+        <p class="content">
+            Là chủ sở hữu chứng chỉ hành nghề số …………………. được cấp bởi sở y tế ……………, phạm vi hoạt động chuyên môn: Khám
+            chữa bệnh chuyên khoa …………………………. Sau khi bàn bạc, thỏa thuận, hai bên thống nhất ký kết hợp đồng hợp tác
+            theo các điều khoản sau:
+        </p>
+    </div>
+
+    <div class="dieukhoan 1">
+        <p class="title">
+            Điều 1: Nội dung hợp tác
+        </p>
+        <p class="content">
+            Bên A mời Bên B hợp tác cùng tổ chức, duy trì hoạt động của Phòng khám tại địa
+            chỉ:.......................... Công việc bao gồm: Bên B đứng tên và quản lý Phòng khám chuyên khoa ………………../
+            chuyên khoa ………………. trong phòng khám đa khoa Bên B giữ chức vụ
+            người phụ trách chuyên môn Phòng khám/ chuyên khoa ngoài giờ hành chính/ toàn thời gian. </br>
+            </br>
+            1.2 Thời gian hoạt động của phòng khám từ … đến … tất cả các ngày trong tuần.
+
+        </p>
+    </div>
+    <div class="dieukhoan 2">
+        <p class="title">
+            Điều 2: Thời hạn hợp tác
+        </p>
+        <p class="content">
+            Thời hạn Bên B hợp tác với Bên A là 01/02/03/05 năm (… năm), được tính từ ngày …. tháng …. năm 2021 đến ngày
+            …. tháng …. năm 2023 (sau đây gọi là “Thời hạn hợp tác”).
+        </p>
+    </div>
+    <div class="dieukhoan 2">
+        <p class="title">
+            Điều 3: Thu nhập và hình thức thanh toán
+        </p>
+        <p class="content">
+            3.1 Thu nhập Bên A trả cho bên B là: ….........................................
+            </br>
+            </br> 3.2 Bên A đặt cọc cho Bên B trước 1 khoản là một tháng/ hai tháng/ ba tháng/ không cọc
+            (................................) để Bên A sử dụng hồ sơ Bên B kiểm tra. Nếu hồ sơ Bên B không Đạt, Bên B
+            hoàn lại khoản phí đã nhận từ Bên A và Bên
+            A hoàn trả lại bộ hồ sơ cho Bên B.</br>
+            </br>
+            3.3 Chu kỳ thanh toán: Bên B được thanh toán 01/03/06 tháng (Sáu tháng)/ 1 năm một lần bằng tiền mặt hoặc
+            qua chuyển khoản sau: Chủ tài khoản:………………………………………. Số tài khoản:………………………………………… Tên ngân hàng:
+            ………………………………………
+            </br>
+            </br>
+            3.4 Thu nhập này bắt đầu được tính sau khi ký hợp đồng/ sau khi có phiếu tiếp nhận của bộ phận một cửa/ từ
+            khi có Biên bản thẩm định Đạt/ sau 30 ngày kể từ khi ký hợp đồng 3.5. Thời gian thanh toán: Trong vòng 10
+            ngày đầu mỗi chu kỳ thanh toán.
+        </p>
+    </div>
+    <div class="dieukhoan 4">
+        <p class="title">
+            Điều 4: Quyền hạn và nghĩa vụ của Bên A
+
+        </p>
+        <p class="content">
+            4.1 Chịu trách nhiệm về việc đầu tư cơ sở vật chất, con người (các bác sỹ, y tá...) đạt tiêu chuẩn đáp ứng
+            theo chất lượng theo đúng quy định của các cơ quan y tế để phòng khám hoạt động tốt nhất. </br>
+            </br> 4.2 Tự chịu trách nhiệm đầu tư chuẩn bị trang thiết bị phòng khám cũng như các chi phí liên quan đến
+            việc xin giấy phép hoạt động của phòng khám với Sở Y tế.</br>
+            </br> 4.3 Tự chịu mọi khoản chi phí liên quan đến việc duy trì đảm bảo hoạt động của phòng khám.</br>
+            </br> 4.4 Cam kết thực hiện đúng theo các danh mục kỹ thuật được Sở y tế cho phép tại phòng khám. </br>
+            </br> 4.5 Có trách nhiệm quản lý, điều hành toàn bộ hoạt động của Phòng khám theo đúng quy định của pháp
+            luật hiện hành.</br>
+            </br> 4.6 Thanh toán đầy đủ và đúng hạn chi phí hợp tác cho Bên B theo Điều 3.
+            </br>
+            </br>
+            </br> 4.7 Bên A có trách nhiệm kê khai, nộp đầy đủ thuế và các nghĩa vụ khác với nhà nước cũng như thanh
+            toán chi phí phát sinh khác từ hoạt động của phòng khám để đảm bảo cho phòng khám hoạt động bình thường. Bên
+            A có trách nhiệm kê khai và
+            nộp thuế thu nhập cá nhân cho bên B theo giá trị của Hợp đồng này.</br>
+            </br> 4.8 Trong thời hạn của hợp đồng, nếu Bên A không thể tiếp tục thực hiện hợp đồng vì những lý do bất
+            khả kháng quy định tại Điều 7 hoặc vì các lý do không mong muốn như phòng khám giải thể hoặc chi nhánh công
+            ty giải thể hoặc các trường
+            hợp khác do pháp luật quy định thì Bên A không phải bồi thường cho bên B khi chấm dứt hợp đồng và Bên B
+            không phải hoàn trả cho Bên A khoản thu nhập đã nhận.
+        </p>
+    </div>
+    <div class="dieukhoan 5">
+        <p class="title">
+            Điều 5: Quyền hạn và trách nhiệm của Bên B
+        </p>
+        <p class="content">
+            5.1 Cung cấp đầy đủ cho bên A các loại giấy tờ, bằng cấp, chứng chỉ liên quan đảm bảo đủ thủ tục để sở y tế
+            …………………….. cấp phép cho phòng khám hoạt động.</br>
+            </br> 5.2 Được nhận chi phí hợp tác do Bên A chi trả theo Điều 3.</br>
+            </br>5.3 Hỗ trợ Bên A các về thủ tục giấy giấy phép phòng khám và cung cấp các thông tin cá nhân khác phục
+            vụ cho hoạt động xin giấy phép phòng khám. </br>
+            </br> 5.4 Sẵn sàng có mặt và phối hợp với Bên A để làm việc với các cơ quan chức năng khi có sự cố hoặc yêu
+            cầu công việc của phòng khám.
+            </br>
+            </br>
+            </br>5.5 Có trách nhiệm phối hợp cùng với Bên A giải quyết các sự cố phòng khám. Chịu trách nhiệm những công
+            việc cá nhân thực hiện. Chi phí giải quyết các sự cố phòng khám sẽ do Bên A chịu.</br>
+            </br>5.6 Không tiết lộ những thông tin về hợp đồng này hoặc các thông tin khác của Bên A cho các tổ chức, cá
+            nhân khác.</br>
+            </br>5.7 Không chịu trách nhiệm về tất cả các sai sót chuyên môn khi không trực tiếp thăm khám và điều trị.
+        </p>
+        <p class="content highlight">5.8 Điều khoản đơn phương huỷ hợp đồng </p>
+        <p class="content">
+            </br>5.8.1 Trong vòng 01 (Một) năm đầu, hai Bên không được phép đơn phương huỷ hợp đồng (trừ trường hợp bên
+            A không thực hiện đầy đủ nghĩa vụ thanh toán). </br>
+            </br> 5.8.2 Trong mọi trường hợp, nếu Bên B đơn phương chấm dứt Hợp đồng trước thời hạn thì phải thông báo
+            bằng văn bản cho Bên A biết trước ít nhất 03 tháng trước thời hạn để Bên A hoàn tất việc tìm bác sỹ thay thế
+            và làm các thủ tục thay đổi
+            tên người đứng tên chuyên khoa phòng khám tại Sở Y tế cũng như các giấy tờ liên quan khác.
+        </p>
+    </div>
+    <div class="dieukhoan 6">
+        <p class="title">
+            Điều 6: Chấm dứt hợp đồng và thanh lý hợp đồng
+        </p>
+        <p class="content">
+            6.1 Hợp đồng sẽ chấm dứt hiệu lực khi hết thời hạn đã ghi trong Điều 2 của hợp đồng. Khi hết hạn hợp đồng,
+            tùy nhu cầu hai bên sẽ thỏa thuận việc gia hạn hợp đồng hoặc thanh lý hợp đồng. Nếu Bên A có nhu cầu tiếp
+            tục hợp tác và Bên B vẫn muốn hợp tác
+            thì Bên A được ưu tiên tiếp tục hợp đồng theo các điều kiện của 2 bên cùng thoả thuận.</br> 6.2 Bên B sẽ
+            thông báo cho Bên A 03 (ba) tháng trước ngày hết hạn hợp đồng về việc Bên B muốn tiếp tục hợp tác hoặc không
+            hợp tác.
+            Nếu Bên A quyết định mời Bên B tiếp tục đứng tên phòng phám thì sẽ được quyền ưu tiên trước.</br>
+            </br> 6.3 Nếu Bên B không muốn tiếp tục thực hiện hợp đồng nữa thì Bên B có trách nhiệm đợi Bên A hoàn tất
+            việc tìm bác sỹ thay thế và các thủ tục thay đổi tên người đứng tên phòng khám tại Sở Y tế cũng như các giấy
+            tờ với các ban ngành liên
+            quan khác.</br>
+            </br> 6.4 Trong mọi trường hợp Hợp đồng chấm dứt hiệu lực, toàn bộ tài sản liên quan đến Phòng khám do Bên A
+            đầu tư sẽ thuộc về Bên A và do Bên A toàn quyền xử lý.
+        </p>
+    </div>
+    <div class="dieukhoan 7">
+        <p class="title">
+            Điều 7: Các trường hợp bất khả kháng
+        </p>
+        <p class="content">
+            7.1 Bên A hoặc Bên B không bị coi là vi phạm hợp đồng và không bị phạt hoặc không phải chịu trách nhiệm bồi
+            thường thiệt hại nếu việc chậm thực hiện hoặc không thực hiện các nghĩa vụ được các bên thỏa thuận trong hợp
+            đồng này do có sự kiện bất khả kháng
+            như thiên tai, chiến tranh, hỏa hoạn, sự thay đổi quy định pháp luật, người đại diện của hai bên qua đời, và
+            các trường hợp khác mà không phải do lỗi của các Bên gây ra.
+        </p>
+    </div>
+    <div class="dieukhoan 8">
+        <p class="title">
+            Điều 8: Giải quyết tranh chấp Hợp đồng
+        </p>
+        <p class="content">
+            8.1 Hai bên cam kết thực hiện đúng các điều khoản trên. </br>
+            </br> 8.2 Trong thời hạn có hiệu lực của hợp đồng, nếu bên nào muốn chấm dứt  thực hiện hợp đồng trước thời
+            hạn thì phải báo trước cho bên kia theo như quy định tại Điều 4 và Điều 5 của hợp đồng này, phải
+            được sự chấp thuận kèm theo
+            các điều kiện ràng buộc nêu trên.   </br>
+            </br> 8.3 Trong quá trình thực hiện hợp đồng, nếu có gì vướng mắc thì hai bên cùng thỏa thuận, bàn bạc giải
+            quyết trên tinh thần hợp tác. Trong trường hợp tranh chấp không được giải quyết thỏa đáng, vụ việc sẽ được
+            đưa ra giải quyết tại toà
+            án có thẩm quyền.
+        </p>
+    </div>
+    <div class="dieukhoan 9">
+        <p class="title">
+            Điều 9: Hiệu lực Hợp đồng
+        </p>
+        <p class="content">
+            9.1 Hợp đồng có hiệu lực từ ngày ký. Hợp đồng hết hiệu lực khi kết thúc thời hạn hợp tác hoặc xảy ra các
+            trường hợp như đã nêu trong hợp đồng.</br>
+            </br> 9.2 Hợp đồng được lập thành 02 bản. Bên A giữ 01 bản, Bên B giữ 01 bản. Các bản đều có giá trị như
+            nhau.
+        </p>
+    </div>
+    <div class="container">
+        <table border="1" class="table-ky">
+            <tr>
+                <td>
+                    <p>
+                        BÊN A </br>
+                        (Ký và ghi rõ họ tên)
+                    </p>
+                </td>
+                <td>
+                    <p> BÊN B </br>
+                        (Ký và ghi rõ họ tên)</p>
+
+                </td>
+            </tr>
+        </table>
+    </div>
+<!-- Code injected by live-server -->
+<script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script></body>
+
+</html>
